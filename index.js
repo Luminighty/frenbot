f3a0972ddbc55294ac3dc31214d06ac412e9b654
Luminight <balazs922@gmail.hu>
2020-09-19 19:21:22 +0200

    queue

--- index.js
+++ index.js
@@ -2,8 +2,39 @@ const Discord = require("discord.js");
 
 const bot = new Discord.Client();
 
+const prefix = "!";
+
 bot.on("ready", function() {
 	console.log(`${bot.user.username} is online!`);
 });
 
 bot.login(process.env.token);
+
+
+bot.on("message", message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = mesage.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+
+});
+
+const commands = {
+	"q": queue, "queue": queue,
+};
+
+/**
+ * 
+ * @param {*} args 
+ * @param {Discord.Message} message 
+ */
+function queue(args, message) {
+	const role = message.guild.roles.cache.find(role => role.name === 'LFG');
+	if (message.member.roles.cache.some(role => role.name === 'LFG')) {
+		message.member.roles.add(role);
+	} else {
+		message.member.roles.remove(role);
+	}
+}
+
