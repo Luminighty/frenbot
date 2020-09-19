77c407e7917ba079866ca6ebddb5f32f2ed4d082
Luminight <balazs922@gmail.hu>
2020-09-19 19:05:43 +0200

    Init

---
+++ index.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+
+const bot = new Discord.Client();
+
+bot.on("ready", function() {
+	console.log(`${bot.user.username} is online!`);
+});
+
+bot.login(process.env.token);
